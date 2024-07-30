83db30c502108c0ebb44e162db912a00e3302259
Aadipatle <aadipatle6@gmail.com>
2024-07-30 12:23:34 +0530

    new user add

--- controller/controllers.js
+++ controller/controllers.js
@@ -1,24 +1,29 @@
-let users = [];
+let users = [{
+    id: 1,
+    name: 'user',
+    category: 'test',
+    number: 1234567891
+}];
 
-export const getUsers = (req,res)=>{
+export const getUsers = (req, res) => {
     res.send(users)
 }
-export const createUser = (req, res)=>{
+export const createUser = (req, res) => {
     const user = req.body;
     console.log("Received user data:", user);
     users.push({ ...user })
     res.send('new user added successfully')
-} 
-export const getUser = (req,res)=>{
-    const singleUser = users.filter((user)=> user.id === req.params.id)
+}
+export const getUser = (req, res) => {
+    const singleUser = users.filter((user) => user.id === req.params.id)
     res.send(singleUser)
 }
-export const deleteUser = (req,res)=>{
-    users = users.filter((user)=> user.id !== req.params.id)
+export const deleteUser = (req, res) => {
+    users = users.filter((user) => user.id !== req.params.id)
     res.send('user deleted successfully')
 }
-export const updateUser = (req,res)=>{
-    const user = users.find((user)=> user.id === req.params.id)
+export const updateUser = (req, res) => {
+    const user = users.find((user) => user.id === req.params.id)
     user.id = req.body.id
     user.name = req.body.name
     user.category = req.body.category
